d98fcc323eced55f2adfeafcc334d61027a57821
Alpesh Singh <152172503+alpesh502@users.noreply.github.com>
2025-12-29 18:10:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import numpy as np
 import pickle
+import os
 
 app = Flask(__name__)
 
@@ -44,5 +45,10 @@ def predict():
     except Exception as e:
         return f"Error occurred: {e}"
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
+
